b35f4570f0008a1bf556b6b13b6c9cfbf7d0be90
Michael Dunlap <michaeldunlap@earthlink.net>
2024-03-19 22:07:30 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>michaeldaviddunlap.com</h1>
+      <p className="read-the-docs">
+        is down for the count...for now.
+      </p>
+    </>
+  )
+}
+
+export default App
